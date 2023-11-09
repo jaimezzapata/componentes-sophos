27545fb9a2b3613bedbb4db26d180f6a87ef282e
jaimezzapata <zapataval2304@gmail.com>
2023-11-09 16:01:51 -0500

    Se crea proyecto componentes

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Usuarios from "./components/Usuarios";
+
+function App() {
+  return (
+    <section>
+      <Usuarios />
+      <Usuarios />
+      <Usuarios />
+      <Usuarios />
+      <Usuarios />
+      <Usuarios />
+      <Usuarios />
+      <Usuarios />
+      <Usuarios />
+    </section>
+  );
+}
+
+export default App;
---
+++ src/components/Usuarios.jsx
@@ -0,0 +1,18 @@
+const Usuarios = () => {
+    let nombre = 'Karen'
+    return(
+        <section className="usurios">
+            <h1>Listado de Usuarios</h1>
+            <section>
+                <p>Nombre: {nombre}</p>
+                <p>Telefono: 3152001282</p>
+            </section>
+        </section>
+    )
+}
+
+export default Usuarios
+
+// export {
+//     Usuarios
+// }
